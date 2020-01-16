837c66d3919d29502c970f49af4ee624f9a6a8b7
Jessica J Sun <jessi.slm@hotmail.com>
2020-01-16 14:32:58 +1100

    Merge Intervals

--- Array, Primitive & String/mergeIntervals.js
+++ Array, Primitive & String/mergeIntervals.js
@@ -3,18 +3,28 @@
  * @return {number[][]}
  */
 var merge = function(intervals) {
-    if (intervals.length <= 1) return intervals
     let res = []
+    if (intervals.length <= 1) return intervals
+    intervals.sort(sortColumns)
     let interval = intervals[0]
     res.push(interval)
     for (let i=0; i<intervals.length; i++) {
-        if (interval[1] >= intervals[i][0]) {
+        if (intervals[i][0] <= interval[1]) {   //overlapping
             interval[1] = Math.max(interval[1], intervals[i][1])
         } else {
             interval = intervals[i]
             res.push(interval)
         }
     }
+    
     return res
 };
 
+function sortColumns(a, b) {
+    if (a[0] === b[0]) {
+        return 0;
+    }
+    else {
+        return (a[0] < b[0]) ? -1 : 1;
+    }
+}
