439f167562134d6ca9a8704b5c6d8f0ba5a8502b
Jessica J Sun <jessi.slm@hotmail.com>
2020-02-05 22:54:56 +1100

    Reverse List

---
+++ Linked List/reverseList.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    let newHead = null;
+    while(head) {
+        let next = head.next;
+        head.next = newHead;
+        newHead = head;
+        head = next;
+    }
+        
+    return newHead;
+};
