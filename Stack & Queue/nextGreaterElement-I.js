69848e7562f7251bd6f906f525f9d2ad1bce9a1e
Jessica J Sun <jessi.slm@hotmail.com>
2020-03-02 14:26:24 +1100

    Next Greater Element I

---
+++ Stack & Queue/nextGreaterElement-I.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var nextGreaterElement = function(nums1, nums2) {
+    let stack = [], map = new Map()
+    nums2.forEach(num => {
+        while (stack.length && stack[stack.length-1] < num) {
+            map.set(stack.pop(), num)
+        }
+        stack.push(num)
+    })
+    for (let i=0; i<nums1.length; i++) {
+        nums1[i] = map.has(nums1[i]) ? map.get(nums1[i]) : -1
+    }
+    return nums1
+};
